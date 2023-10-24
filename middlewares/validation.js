c1ba01789fdd6d6272b20566ca7ffcd0c0c242a3
AntonMikhailov1 <mikhailovanton.nsk@gmail.com>
2023-10-24 22:13:51 +0300

    feat: add validation

---
+++ middlewares/validation.js
@@ -0,0 +1,60 @@
+const { celebrate, Joi, CelebrateError } = require('celebrate');
+const validator = require('validator');
+
+const urlValidation = (url) => {
+  if (!validator.isURL(url)) {
+    throw new CelebrateError('Некорректный адрес');
+  }
+  return url;
+};
+
+const validateMovie = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().custom(urlValidation).required(),
+  }),
+});
+
+const validateUserId = celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().length(24).hex(),
+  }),
+});
+
+const validateUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
+
+const validateUserUpdate = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+});
+
+const validateAvatar = celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().custom(urlValidation).required(),
+  }),
+});
+
+const validateLogin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
+
+module.exports = {
+  validateCard,
+  validateUserId,
+  validateCardId,
+  validateUser,
+  validateUserUpdate,
+  validateAvatar,
+  validateLogin,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const users = require('./users');
 const movies = require('./movies');
+const { createUser, signInUser, signOutUser } = require('../controllers/users');
 
 // const NotFoundError = require('../errors/NotFoundError');
 
@@ -9,9 +10,9 @@ const auth = require('../middlewares/auth');
 // const { createUser, login } = require('../controllers/users');
 // const { validateUser, validateLogin } = require('../middlewares/validation');
 
-// router.post('/signup', validateUser, createUser);
-// router.post('/signin', validateLogin, signInUser);
-// router.post('/signout', signOutUser);
+router.post('/signup', validateUser, createUser);
+router.post('/signin', validateLogin, signInUser);
+router.post('/signout', signOutUser);
 
 router.use('/users', auth, users);
 router.use('/cards', auth, movies);
